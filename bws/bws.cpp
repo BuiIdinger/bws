4dd27ff6d1246fd38d20f73a737fd864e0a98331
Shayden Seymour <shaydenseymour@gmail.com>
2024-11-16 20:39:56 +1300

    Remove std::cout and change return statement

--- bws/bws.cpp
+++ bws/bws.cpp
@@ -1,6 +1,10 @@
-﻿#include "bws.h"
+﻿/*
+ * Copyright (C) Buildinger
+ */
+
+#include "bws.h"
 
 int main(int argc, char* const* argv) {
-	std::cout << "Hello CMake." << "\n";
-	return 0;
+
+	return BWS_ABORT;
 }
